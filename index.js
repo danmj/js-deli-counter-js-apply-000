404daa336d5f4327167f52240228a36a10ab9362
danmj <daniel.jung@stern.nyu.edu>
2018-07-10 16:31:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ function nowServing(katzDeliLine) {
 var line = []
 
 function currentLine(katzDeliLine) {
+  let i = 0
+    while(i < katzDeliLine.length) {
+      line.push(' ' + [i + 1] + '. '+ katzDeliLine[i])
+      i++
   if(katzDeliLine.length === 0) {
     return("The line is currently empty.")
   }
   else {
-    let i = 0
-    while(i < katzDeliLine.length) {
-      line.push(' ' + [i + 1] + '. '+ katzDeliLine[i])
-      i++
+    
     return("The line is currently: " + line)
     }
   }
