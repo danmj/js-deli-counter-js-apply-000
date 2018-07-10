a7600e738711617959429b313a1d2605a195f2da
danmj <daniel.jung@stern.nyu.edu>
2018-07-10 15:52:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  var position = katzDeliLine.length+1;
+  katzDeliLine.push(name);
+  return("Welcome, " + name + ". You are number " + position + " in line.")
+}
+
+function nowServing() {
+  
+}
